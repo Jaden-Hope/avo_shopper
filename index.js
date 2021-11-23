528407faff455a3605d9b94a84c94fe7176f60bc
Jaden Hope <Jaden_Hope@Yahoo.com>
2021-11-23 15:02:23 +0200

    Travis Push

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
+const session = require('express-session')
+const avoFactory = require('./avo-shopper');
+const { Pool } = require('pg');
 
 const app = express();
-const PORT =  process.env.PORT || 3019;
+
+const connectionString = 'postgres://xvrlmxrgdqkbqc:764b52b7b3ca1e5ef958fca109b240566a57d87f76a663c071dcc5b1764c4fe7@ec2-63-32-12-208.eu-west-1.compute.amazonaws.com:5432/d6qbra6gk27qop';
+
+const pool = new Pool({
+	connectionString,
+	ssl: {
+		rejectUnauthorized: false,
+	}
+})
+
+pool.connect();
+
+const factory = avoFactory(pool);
 
 // enable the req.body object - to allow us to use HTML forms
 app.use(express.json());
@@ -12,19 +28,56 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 // add more middleware to allow for templating support
-
-app.engine('handlebars', exphbs.engine());
+app.engine('handlebars', engine({ defaultLayout: 'main', layoutsDir: `${__dirname}/views/layouts` }));
 app.set('view engine', 'handlebars');
 
-let counter = 0;
-
-app.get('/', function(req, res) {
-	res.render('index', {
-		counter
-	});
+app.get('/', async (req, res) => {
+	res.render('index', { topDeals: await factory.topFiveDeals() });
 });
 
+app.get('/show/:name/:shop', async (req, res) => {
+	const deals = await factory.dealsForShop(req.params.shop++);
+	res.render('index', { deals, name: req.params.name });
+})
+
+app.post('/shops', async (req, res) => {
+	let input = req.body.addShopIn;
+	let result = input[0].toUpperCase() + input.slice(1).toLowerCase();
+	await factory.createShop(result)
+	res.redirect('/shops');
+})
+
+app.get('/shops', async (req, res) => {
+	const shopList = await factory.listShops();
+	res.render('shopsList', { shopList });
+})
+
+app.get('/addShop', async (req, res) => {
+	res.render('addShops');
+})
+
+app.get('/newDeal', async (req, res) => {
+	const shops = await factory.listShops();
+	res.render('addDeals', { shops });
+})
+
+app.post('/newDeal', async (req, res) => {
+	await factory.createDeal(req.body.shops, req.body.newDealQty, req.body.newDealPrice);
+	res.redirect('/');
+})
+
+app.get('/search', async (req, res) => {
+	res.render('searchShops')
+})
+
+app.post('/search', async (req, res) => {
+	const searchResults = await factory.recommendDeals(req.body.budget);
+	res.render('searchShops', { searchResults })
+})
+
 // start  the server and start listening for HTTP request on the PORT number specified...
-app.listen(PORT, function() {
+const PORT = process.env.PORT || 3019;
+
+app.listen(PORT, function () {
 	console.log(`AvoApp started on port ${PORT}`)
 });
